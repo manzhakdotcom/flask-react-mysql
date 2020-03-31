6af18b606163d50db430b64cec0362df19973464
Manzhak <manzhak.com@gmail.com>
2020-03-31 16:03:01 +0300

    add companents

---
+++ src/containers/Archive/Archive.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import classname from './Archive.css'
+
+class Archive extends Component {
+    render() {
+        return (
+            <div className={classname.Archive}>
+                <h1>Archive</h1>
+            </div>
+        );
+    }
+}
+
+export default Archive
